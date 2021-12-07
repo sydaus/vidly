d201c5394503f56204789e0f3f28317d549e7bc5
sydaus <george.lujun.yang@gmail.com>
2021-12-07 12:37:36 +1100

    remove the temporary code in like.jsx and movies.jsx

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -89,45 +89,6 @@ class Movies extends React.Component {
       );
     }
   }
-
-  // render() {
-  //     return (
-  //         <React.Fragment>
-  //         <span>{ this.moviesNumber()}</span>
-  //         <table className="table">
-  //             <thead>
-  //                 <tr>
-  //                     <th>Title</th>
-  //                     <th>Genre</th>
-  //                     <th>Stock</th>
-  //                     <th>Rate</th>
-  //                     <th></th>
-  //                 </tr>
-  //             </thead>
-  //             <tbody>
-  //                 {this.state.movies.map(movie=> (
-  //                     <tr key={movie._id}>
-  //                         <td>{movie.title}</td>
-  //                         <td>{movie.genre.name}</td>
-  //                         <td>{movie.numberInStock}</td>
-  //                         <td>{movie.dailyRentalRate}</td>
-  //                         <td><button
-  //                         onClick={()=>this.handleDelete(movie)}
-  //                         className="btn btn-danger btn-sm m2" >Delete</button></td>
-  //                     </tr>
-  //                 ))}
-  //             </tbody>
-  //         </table>
-  //         </React.Fragment>
-  //     );
-
-  // }
-
-  // moviesNumber() {
-  //     const value = this.state.movies.length;
-  //     console.log(value);
-  //     return value === 0 ? "there are no movies in the list" : "showing " + value + " movies in the database";
-  // };
 }
 
 export default Movies;
